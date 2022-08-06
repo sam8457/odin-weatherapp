426d33eeaede34e983ce8ca8b48337d3b5a12fd4
sam8457 <samjlumpy@gmail.com>
2022-08-06 09:34:02 -0500

    Add SVGs for weather icons, change their colors to white

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-
-async function getWeatherFor(location) {
-    let weatherResponse = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=f540c46c7826a6b13837d9d586e2e08b`, {mode: "cors"});
-
-    console.log(weatherResponse);
-}
-
-function updateCityName() {
+function getCityName() {
     const search = document.getElementById("city");
     let location = search.value;
+    return location;
+}
+
+async function getWeatherFor(location) {
+    const weatherResponse = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=f540c46c7826a6b13837d9d586e2e08b`, {mode: "cors"});
+    const weatherData = await weatherResponse.json();
+    console.log(weatherData);
+    console.log(weatherData.weather[0].main);
+}
+
+function updateResultsPane() {
+    
+}
+
+function updateCityName(location) {
     const cityName = document.getElementById("city-name");
     cityName.textContent = `${location}`;
 }
 
 const searchButton = document.getElementById("search");
 searchButton.addEventListener("click", (event) => event.preventDefault()); // don't reload page on click
-searchButton.addEventListener("click", () => updateCityName());
+searchButton.addEventListener("click", async () => {
+    let location = getCityName();
+    updateCityName(location);
+    // getWeatherFor(location);
+});
