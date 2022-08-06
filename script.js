9a89837850da1bb33862948ed95134b1cfdaed8f
sam8457 <samjlumpy@gmail.com>
2022-08-06 14:59:34 -0500

    Finish getWeatherFor() function

--- script.js
+++ script.js
@@ -7,15 +7,43 @@ function getCityName() {
 async function getWeatherFor(location) {
     const weatherResponse = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&APPID=f540c46c7826a6b13837d9d586e2e08b`, {mode: "cors"});
     const weatherData = await weatherResponse.json();
-    console.log(weatherData);
-    console.log(weatherData.weather[0].main);
+    
+    const results = document.getElementById("results");
+    const resultsList = results.childNodes;
+
+    // change temp/weather
+    resultsList[1 *2+1].textContent = weatherData.weather[0].main;
+    resultsList[2 *2+1].textContent = `${Math.round(weatherData.main.temp)}°F`;
+    resultsList[6 *2+1].textContent = `${Math.round(weatherData.main.temp_min)}°F`;
+    resultsList[7 *2+1].textContent = "—";
+    resultsList[8 *2+1].textContent = `${Math.round(weatherData.main.temp_max)}°F`;
+
+    // change days
+    const currentDate = new Date();
+    const weekday = currentDate.toLocaleString("default", {weekday: "short"});
+    const month = currentDate.toLocaleString("default", {month: "short"});
+    const date = currentDate.getDate();
+    resultsList[3 *2+1].textContent = weekday;
+    resultsList[4 *2+1].textContent = month;
+    resultsList[5 *2+1].textContent = `${date}`;   
+
+    // change weather icon
+    const weatherIcons = {
+        "Clear":"./Images/sunny_FILL0_wght400_GRAD0_opsz48.svg",
+        "Rain":"./Images/rainy_FILL0_wght400_GRAD0_opsz48.svg",
+        "Thunderstorm":"./Images/thunderstorm_FILL0_wght400_GRAD0_opsz48.svg",
+        "Snow":"./Images/cloudy_snowing_FILL0_wght400_GRAD0_opsz48.svg",
+        "Cloudy":"./Images/cloudy_FILL0_wght400_GRAD0_opsz48.svg",
+    };
+    resultsList[0 *2+1].src // change source
 }
 
+// for testing only
 function updateResultsPane() {
     const results = document.getElementById("results");
     const resultsList = results.childNodes;
 
-    // nodelists indexed by [nth_element *2+1]
+    // nodelists indexed by [nth_element * 2 + 1]
     resultsList[1 *2+1].textContent = "Cloudy";
     resultsList[2 *2+1].textContent = "23c";
     resultsList[3 *2+1].textContent = "";
@@ -31,6 +59,6 @@ searchButton.addEventListener("click", (event) => event.preventDefault()); // do
 searchButton.addEventListener("click", async () => {
     let location = getCityName();
     updateCityName(location);
-    updateResultsPane();
-    // getWeatherFor(location);
+    // updateResultsPane();
+    getWeatherFor(location);
 });
