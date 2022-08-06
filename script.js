08bdc28f1d85d3df36b146fc3d33bb4a9793973c
sam8457 <samjlumpy@gmail.com>
2022-08-06 10:13:41 -0500

    Create central box for results to go in, add dummy data

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ function getCityName() {
 }
 
 async function getWeatherFor(location) {
-    const weatherResponse = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=f540c46c7826a6b13837d9d586e2e08b`, {mode: "cors"});
+    const weatherResponse = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&APPID=f540c46c7826a6b13837d9d586e2e08b`, {mode: "cors"});
     const weatherData = await weatherResponse.json();
     console.log(weatherData);
     console.log(weatherData.weather[0].main);
 }
 
 function updateResultsPane() {
-    
+
 }
 
 function updateCityName(location) {
