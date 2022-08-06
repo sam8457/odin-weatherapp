e69511727c19d31d5fd6e61eae61abf98be491e8
sam8457 <samjlumpy@gmail.com>
2022-08-05 22:46:14 -0500

    Style results pane

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 
 async function getWeatherFor(location) {
-    weatherResponse = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=f540c46c7826a6b13837d9d586e2e08b`, {mode: "cors"});
+    let weatherResponse = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=f540c46c7826a6b13837d9d586e2e08b`, {mode: "cors"});
 
     console.log(weatherResponse);
 }
 
-//getWeatherFor("london");
+function updateResults() {
+    const search = document.getElementById("search");
+    const results = document.getElementById("results");
+    results.innerHTML();
+}
+
+const searchButton = document.getElementById("search");
+searchButton.addEventListener("click", (event) => event.preventDefault()); // don't reload page
+searchButton.addEventListener("click", () => console.log("searched")); // 
