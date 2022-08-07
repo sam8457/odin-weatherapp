c11fbf0591661064cd378ce580b7ddb600a54461
sam8457 <samjlumpy@gmail.com>
2022-08-07 16:43:48 -0500

    Redesign results pane

--- script.js
+++ script.js
@@ -4,58 +4,67 @@ function getCityName() {
     return location;
 }
 
+function updateCityName(location) {
+    const cityName = document.getElementById("city-name");
+    cityName.textContent = `${location}`;
+}
+
 async function getWeatherFor(location) {
-    const weatherResponse = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&APPID=f540c46c7826a6b13837d9d586e2e08b`, {mode: "cors"});
-    const weatherData = await weatherResponse.json();
-    
-    const results = document.getElementById("results");
-    const resultsList = results.childNodes;
+    // const weatherResponse = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&APPID=f540c46c7826a6b13837d9d586e2e08b`, {mode: "cors"});
+    // const weatherData = await weatherResponse.json();
 
-    // change temp/weather
-    const weatherDescription = weatherData.weather[0].main;
-    const currentTemp = Math.round(weatherData.main.temp);
-    const hiTemp = Math.round(weatherData.main.temp_max);
-    const loTemp = Math.round(weatherData.main.temp_min);
-    resultsList[1 *2+1].textContent = weatherDescription;
-    resultsList[2 *2+1].textContent = `${currentTemp}°F`;
-    resultsList[6 *2+1].textContent = `${loTemp}°F`;
-    resultsList[7 *2+1].textContent = "—";
-    resultsList[8 *2+1].textContent = `${hiTemp}°F`;
+    // const weatherNameResult = weatherData.weather[0].main;
+    const weatherName = "Clear";
+    const weatherNameContent = document.querySelector("#weather-name > div.content");
+    weatherNameContent.textContent = weatherName;
 
-    // change days
-    const currentDate = new Date();
-    const weekday = currentDate.toLocaleString("default", {weekday: "short"});
-    const month = currentDate.toLocaleString("default", {month: "short"});
-    const date = currentDate.getDate();
-    resultsList[3 *2+1].textContent = weekday;
-    resultsList[4 *2+1].textContent = month;
-    resultsList[5 *2+1].textContent = `${date}`;   
-
-    // change weather icon
-    const weatherIcons = {
+    const weatherIconsList = {
         "Clear":"./Images/sunny_FILL0_wght400_GRAD0_opsz48.svg",
         "Rain":"./Images/rainy_FILL0_wght400_GRAD0_opsz48.svg",
         "Thunderstorm":"./Images/thunderstorm_FILL0_wght400_GRAD0_opsz48.svg",
         "Snow":"./Images/cloudy_snowing_FILL0_wght400_GRAD0_opsz48.svg",
         "Clouds":"./Images/cloudy_FILL0_wght400_GRAD0_opsz48.svg",
     };
-    resultsList[0 *2+1].src = weatherIcons[weatherDescription];
-}
+    const weatherIcon = document.querySelector("#weather-icon");
+    weatherIcon.src = weatherIconsList[weatherName];
 
-// for testing only
-function updateResultsPane() {
-    const results = document.getElementById("results");
-    const resultsList = results.childNodes;
+    // const temp = Math.round(weatherData.main.temp);
+    const temp = "45";
+    const tempContent = document.querySelector("#temp > div.content");
+    tempContent.textContent = `${temp}°F`;
 
-    // nodelists indexed by [nth_element * 2 + 1]
-    resultsList[1 *2+1].textContent = "Cloudy";
-    resultsList[2 *2+1].textContent = "23c";
-    resultsList[3 *2+1].textContent = "";
-}
+    // const temp = Math.round(weatherData.main.feels_like);
+    const feelsLike = "40";
+    const feelsLikeContent = document.querySelector("#feels-like > div.content");
+    feelsLikeContent.textContent = `${feelsLike}°F`;
 
-function updateCityName(location) {
-    const cityName = document.getElementById("city-name");
-    cityName.textContent = `${location}`;
+    // const tempMin = Math.round(weatherData.main.temp_min);
+    const tempMin = "30";
+    const tempMinContent = document.querySelector("#temp-min > div.content");
+    tempMinContent.textContent = `${tempMin}°F`;
+
+    // const tempMax = Math.round(weatherData.main.temp_max);
+    const tempMax = "50";
+    const tempMaxContent = document.querySelector("#temp-max > div.content");
+    tempMaxContent.textContent = `${tempMax}°F`;
+
+    // const weatherDesc = weatherData.weather[0].description;
+    const weatherDesc = "few clouds";
+    const weatherDescContent = document.querySelector("#weather-desc > div.content");
+    weatherDescContent.textContent = weatherDesc;
+
+    
+
+    /*
+    // change days
+    const currentDate = new Date();
+    const weekday = currentDate.toLocaleString("default", {weekday: "short"});
+    const month = currentDate.toLocaleString("default", {month: "short"});
+    const date = currentDate.getDate();
+    resultsList[2].textContent = weekday;
+    resultsList[3].textContent = month;
+    resultsList[4].textContent = `${date}`;   
+    */
 }
 
 const searchButton = document.getElementById("search");
@@ -63,6 +72,5 @@ searchButton.addEventListener("click", (event) => event.preventDefault()); // do
 searchButton.addEventListener("click", async () => {
     let location = getCityName();
     updateCityName(location);
-    // updateResultsPane();
     getWeatherFor(location);
 });
