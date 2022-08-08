9a6099ae464d3aa604717b6af851948836b94e04
sam8457 <samjlumpy@gmail.com>
2022-08-07 21:18:36 -0500

    Finish forecast function

--- script.js
+++ script.js
@@ -48,24 +48,6 @@ async function getWeatherFor(location) {
     tempMaxContent.textContent = `${tempMax}°F`;
 
     /*
-        TODO:
-            *Add forecast functionality
-            *Apace out forecast label or remove entirely
-            *Style searchbox and button for browser consitency (especially on chrome)
-            *Make it look nice
-            *Add windspeed, humidity
-    */
-
-    //const forecastResponse = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${location}&units=imperial&APPID=f540c46c7826a6b13837d9d586e2e08b`, {mode: "cors"});
-    //const forecastData = await forecastResponse.json();
-
-    `<div class="day-container">
-        <div class="weekday">Monday</div>
-        <img class="day-icon" src="./Images/sunny_FILL0_wght400_GRAD0_opsz48.svg">
-        <div class="day-temp">87f</div>
-    </div>`
-
-    /*
     // change days
     const currentDate = new Date();
     const weekday = currentDate.toLocaleString("default", {weekday: "short"});
@@ -77,10 +59,65 @@ async function getWeatherFor(location) {
     */
 }
 
+async function getForecastFor(location) {
+    const forecastResponse = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${location}&units=imperial&APPID=f540c46c7826a6b13837d9d586e2e08b`, {mode: "cors"});
+    const forecastData = await forecastResponse.json();
+
+    const forecastContainer = document.querySelector("#forecast > div.content");
+
+    const numForecastDays = 5;
+    for (let i = 0; i < numForecastDays; i++) {
+        
+        const dateRaw = forecastData.list[i * 8 + 4].dt_txt; // [nth_day * 8 + 4] gets weather @ 12:pm each day
+        const dateObj = new Date(dateRaw);
+        const dayOfWeekNum = dateObj.getDay();
+        const daysOfWeek = {
+            0: "Sun",
+            1: "Mon",
+            2: "Tue",
+            3: "Wed",
+            4: "Thurs",
+            5: "Fri",
+            6: "Sat"
+        };
+        const dayOfWeekName = daysOfWeek[dayOfWeekNum];
+
+        const weatherName = forecastData.list[i * 8 + 4].weather[0].main;
+        const weatherIconsList = {
+            "Clear":"./Images/sunny_FILL0_wght400_GRAD0_opsz48.svg",
+            "Clouds":"./Images/cloudy_FILL0_wght400_GRAD0_opsz48.svg",
+            "Rain":"./Images/rainy_FILL0_wght400_GRAD0_opsz48.svg",
+            "Snow":"./Images/cloudy_snowing_FILL0_wght400_GRAD0_opsz48.svg",
+            "Thunderstorm":"./Images/thunderstorm_FILL0_wght400_GRAD0_opsz48.svg",
+        };
+        const weatherIcon = weatherIconsList[weatherName];
+
+        const temp = Math.round(forecastData.list[i * 8 + 4].main.temp);
+
+        const forecastDay = document.createElement("div");
+        forecastDay.innerHTML = `
+        <div class="day-container">
+            <div class="weekday">${dayOfWeekName}</div>
+            <img class="day-icon" src="${weatherIcon}">
+            <div class="day-temp">${temp}°F</div>
+        </div>`;
+        forecastContainer.appendChild(forecastDay);
+    }
+}
+
 const searchButton = document.getElementById("search");
 searchButton.addEventListener("click", (event) => event.preventDefault()); // don't reload page on click
 searchButton.addEventListener("click", async () => {
     let location = getCityName();
     updateCityName(location);
     getWeatherFor(location);
+    getForecastFor(location);
 });
+
+/*
+    TODO:
+        *Add forecast functionality
+        *Style searchbox and button for browser consitency (especially on chrome)
+        *Make it look nice
+        *Add windspeed, humidity
+*/
